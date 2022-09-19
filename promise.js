124c7582333793c0b3f3c4507a5f2a3ee3efe939
frf-12 <fauzirahmanf@gmail.com>
2022-09-19 13:19:39 +0700

    coba coba

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaksi) => {
+
+  const data1 = await promiseTheaterIXX();
+  const data2 = await promiseTheaterVGC();
+
+  let total = [].concat(data1, data2);
+
+  let nilai=0;
+
+  total.forEach((data) => {
+    if (data.hasil == reaksi) {
+      nilai++;
+    }
+  });
+
+  return nilai;
+};
 
 module.exports = {
   promiseOutput,
